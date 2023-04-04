846238bb0723e34f9c413956d6c4eaadc2c21bf8
Taylor Schrader <28935307+Taylorschrader34@users.noreply.github.com>
2023-04-04 17:16:56 -0500

    basic header and navigation

---
+++ src/components/header/navigation/Navigation.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+import FullLogo from "../../../assets/images/example.png";
+
+export const Navigation = (props) => {
+  const buttonStyle = {
+    textDecoration: "none",
+  };
+
+  return (
+    <div className="header">
+      <div className="container">
+        <div className="logo">
+          <Link to={"/"}>
+            <img src={FullLogo} />
+          </Link>
+        </div>
+        <div className="navigation">
+          <ul>
+            <li>
+              <Link to="/" style={buttonStyle}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/reports" style={buttonStyle}>
+                Reports
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Navigation.propTypes = {};
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom";
-// import { Header } from "../components/Header/Header";
+ import { Header } from "../components/header/Header";
 
 const Layout = () => {
   return (
     <>
-      {/* <Header></Header> */}
+      <Header></Header>
       <Outlet />
     </>
   );
